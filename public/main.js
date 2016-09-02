0fa5c33aa99bbaa90122107ada042279f14032ee
Dmitry Sannikov <sdk96@mail.ru>
2016-09-02 10:40:43 -0700

    refactoring

---
+++ public/main.js
@@ -0,0 +1,11 @@
+function onSubmit(form) {
+    let data = {
+        user: form.elements['user'].value,
+        email: form.elements['email'].value
+    };
+    let result = request('/users', data);
+    if (result == '0') {
+        form.hidden = true;
+    }
+    console.log(data, result);
+}
